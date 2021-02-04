567ff925149303d6527a50802e755ffb9a8f2ac2
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2021-02-04 15:42:56 +0100

    Comments

--- CoinToss.py
+++ CoinToss.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+# seed(): sets the random seed, so that your results are reproducible between simulations.
 np.random.seed(123)
 
+# As an argument, it takes an integer of your choosing. If you call the function, no output will be generated.
+
 coin = np.random.randint(0, 2)  # This randomly generates 0 or 1
+# rand(): if you don't specify any arguments, it generates a random float between zero and one.
+
 
 if coin == 0:
     print("heads")
