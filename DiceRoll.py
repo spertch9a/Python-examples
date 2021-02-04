fedae7bb9c3421e706d257bf2de5801986db2973
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2021-02-04 15:48:47 +0100

    Diceroll.py

---
+++ DiceRoll.py
@@ -0,0 +1,11 @@
+# import numpy and set the seed
+import numpy as np
+
+# try setting the seed to seed() , you'll notice that the results are more convenient
+np.random.seed(123)
+
+# printing the result of the first roll
+print(np.random.randint(1, 7))
+
+# Printing result of second roll
+print(np.random.randint(1, 7))
